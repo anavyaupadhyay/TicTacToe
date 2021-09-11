9c40473b5733ec3b831c1b95b00e2a1d2e348000
useranavya <anavyau@outlook.com>
2021-09-12 01:51:54 +0530

    with some changes

--- tictactoe.py
+++ tictactoe.py
@@ -4,33 +4,38 @@ def printb():
     t=0
     for x in range(9):
         t+=1
-        print(board[str(x)], end = '  '),
+        print(board[str(x)], end = ''),
         if t==3:
-            print(' ')
-            print(' ')
+            print()
             t = 0
+        elif t!=3 and t!=0:
+            print(' | ', end = '')
 def checkb(p):
     global i
     for t in range(0,7,3):
         if (board[str(t)] == board[str(t+1)] == board[str(t+2)] == 'X') or (board[str(t)] == board[str(t+1)] == board[str(t+2)] == 'O'):
+            printb()
             print('Winner of the match is Player {} '.format(p))
+            
             i=0
             
              
     for t in (0,1,2):
         if (board[str(t)] == board[str(t+3)] == board[str(t+6)] == 'X' )or (board[str(t)] == board[str(t+3)] == board[str(t+6)] == 'O'):
+            printb()
             print('Winner of the match is Player {} '.format(p))
-            i=0
-            
-            
+            i=0 
+
     if (board[str(0)] == board[str(4)] == board[str(8)] == 'X') or (board[str(0)] == board[str(4)] == board[str(8)] =='O'):
-            print('Winner of the match is Player {} '.format(p))
-            i=0
-            
+        printb()
+        print('Winner of the match is Player {} '.format(p))
+        i=0
 
     if (board[str(2)] == board[str(4)] == board[str(6)] == 'X') or (board[str(2)] == board[str(4)] == board[str(6)] == 'O'):
-            print('Winner of the match is Player {} '.format(p))
-            i = 0                   
+        printb()
+        print('Winner of the match is Player {} '.format(p))
+        i = 0  
+                         
 def inpb(inp,P):
     if P =='A':
         board[inp] = 'X'
@@ -42,11 +47,14 @@ while i:
     inp = input('Player A:')
     inpb(inp,'A')
     checkb('A')
-    printb()
     if i:
+        printb()
         inp = input('Player B:')
         inpb(inp,'B')
         checkb('B')
-        printb()        
+        if i:
+            printb()
+
+                
 print('Game Over!')
     

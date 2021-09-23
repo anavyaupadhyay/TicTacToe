76819d95ec42609d50d6948694f74867b76e0668
useranavya <anavyau@outlook.com>
2021-09-23 06:27:29 +0530

    Software completed basic changes may require

--- tictactoe.py
+++ tictactoe.py
@@ -10,16 +10,15 @@ def printb():
             t = 0
         elif t!=3 and t!=0:
             print(' | ', end = '')
+    print('')
 def checkb(p):
     global i
     for t in range(0,7,3):
         if (board[str(t)] == board[str(t+1)] == board[str(t+2)] == 'X') or (board[str(t)] == board[str(t+1)] == board[str(t+2)] == 'O'):
             printb()
             print('Winner of the match is Player {} '.format(p))
-            
             i=0
-            
-             
+        
     for t in (0,1,2):
         if (board[str(t)] == board[str(t+3)] == board[str(t+6)] == 'X' )or (board[str(t)] == board[str(t+3)] == board[str(t+6)] == 'O'):
             printb()
@@ -34,14 +33,20 @@ def checkb(p):
     if (board[str(2)] == board[str(4)] == board[str(6)] == 'X') or (board[str(2)] == board[str(4)] == board[str(6)] == 'O'):
         printb()
         print('Winner of the match is Player {} '.format(p))
-        i = 0  
-                         
+        i = 0                          
 def inpb(inp,P):
     if P =='A':
         board[inp] = 'X'
     if P =='B':
         board[inp] = 'O'
-printb()
+def replay():
+    choice = input('Play Again? Enter y or n')
+    return choice == 'y'
+print('\n\n\nWelcome to the TICTACTOE')
+print('------------------------')
+print('\nPosition keys of board:-->')
+print('\n 0 | 1 | 2 \n 3 | 4 | 5 \n 6 | 7 | 8\n')
+
 while i:
     
     inp = input('Player A:')
@@ -54,7 +59,11 @@ while i:
         checkb('B')
         if i:
             printb()
-
+    if not i:
+        if replay():
+            board = {'0':'*','1':'*','2':'*','3':'*','4':'*','5':'*','6':'*','7':'*','8':'*'}
+            i+=1
+    
                 
 print('Game Over!')
     
